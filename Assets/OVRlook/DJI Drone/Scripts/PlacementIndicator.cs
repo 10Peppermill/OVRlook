c27e096a0cde5ecf5abc627db88c7f7a6791d478
10Peppermill <51335910+10Peppermill@users.noreply.github.com>
2021-11-05 13:01:52 -0700

    Placement Sphere fix
    fixed issue with hull reference when trying to change material color

--- Assets/OVRlook/DJI Drone/Scripts/PlacementIndicator.cs
+++ Assets/OVRlook/DJI Drone/Scripts/PlacementIndicator.cs
@@ -4,24 +4,27 @@ using UnityEngine;
 
 public class PlacementIndicator : MonoBehaviour
 {
-    private Renderer rend;
+    //private Renderer rend;
+    public Renderer rend;
     // Start is called before the first frame update
     void Start()
     {
-        Renderer rend = GetComponent<Renderer>();
-        rend.material.shader = Shader.Find("HDRenderPipeline/Lit");
+        //Renderer rend = GetComponent<Renderer>();
+        //rend.material.shader = Shader.Find("Universal Render Pipeline/Lit");
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        Color errorRed = new Color(255, 0, 0, 60);
+        Color errorRed = new Color(1, 0, 0, 0.2f);
 
-        rend.material.SetColor("_BaseMap", errorRed);
+        rend.material.SetColor("_BaseColor", errorRed);
+        //rend.material.SetColor("Color", errorRed);
     }
     private void OnCollisionExit(Collision collision)
     {
-        Color goodGreen = new Color(0, 255, 0, 60);
+        Color goodGreen = new Color(0, 1, 0, 0.2f);
 
-        rend.material.SetColor("_BaseMap", goodGreen);
+        rend.material.SetColor("_BaseColor", goodGreen);
+        //rend.material.SetColor("Color", goodGreen);
     }
 }
